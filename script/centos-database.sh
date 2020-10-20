af0bc89e00fbee2f2bd9e8893801c2047fa83ce4
Martin Silkjær <ms@hjem.lan>
2020-10-20 13:25:49 +0200

    added correction for mysql instead of mariadb

--- script/centos-database.sh
+++ script/centos-database.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+
+rpm -ivh mysql80-community-release-el7-3.noarch.rpm yum update
+
 yum install -y mysql mysql-server mysql-devel
 
 chkconfig --add mysqld
